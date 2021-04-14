172fadf375aa642ee3e514e40904cd695cd4e924
Shubham <busin@DESKTOP-MGMPVCP.(none)>
2021-04-14 12:35:42 +0530

    Menu Inflater

---
+++ app/src/main/java/com/example/menudemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.menudemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.settings:
+                Log.i("Item Selected", "Settings");
+                return true;
+            case R.id.help:
+                Log.i("Item Selected", "Help");
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
